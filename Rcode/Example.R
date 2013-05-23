5e46da1bbd1aed049e33b251f322a565df2af3fc
liang <sallylia@gmail.com>
2013-05-23 00:44:46 -0500

    update Example code

--- Rcode/Example.R
+++ Rcode/Example.R
@@ -14,14 +14,56 @@ for(index in 1:totl){
   YY[YYi,YYj] = as.numeric(DTM[3]$v[index])
 }
 
+# number of documents
+N = dim(YY)[1];
+
+# number of terms
+P = dim(YY)[2];
+
+## number of topics 
+Ntopic = 5;
+K = Ntopic;
+
+# prior
+
+## Construct Prior Here
+V = diag(1, K)
+V0 = diag(1,K)
+a = 0.01
+niu_a = 10
+niu_alpha = 10
+niu = rep(0.98,K)
+q = rep(0.9,K)
+h = 1
+c_s = 1
+d_s = 1
+
+testPrior = list(V,V0,a,niu_a,niu_alpha,niu,q,h,c_s,d_s);
+names(testPrior) = c("V","V0","a","niu_a","niu_alpha","niu","q","h","c_s","d_s");
+
+## Construct Initial for Gibbs Sampling  
+## Can start with the final iteration of last run
+## B = result$BBn[,,Ndraw]...
+
+B = matrix(rnorm(P*K,0,1), nrow=P, ncol=K)
+f = matrix(rnorm(N*K,0,1), nrow=N, ncol=K)
+alpha = rnorm(P, 0, niu_alpha)
+gamma = rnorm(N, 0, 1)
+
+testInitial = list(B,f,alpha,gamma);
+names(testInitial) = c("B","f","alpha","gamma");
+
+
+
+
+
 ## number of iterations to be burn
 Nburn = 0;
 ## number of iterations to be drawn
 Ndrawn = 1;
-## number of topics 
-Ntopic = 5;
 
-test1 = NBfactor(YY, Ntopic, Ndrawn, Nburn);
+
+test1 = NBfactor(YY, testPrior,testInitial,Ntopic, Ndrawn, Nburn);
 
 BBn = test1$BBn;  # Store B matrix from Ndrawn     P*K*Ndrawn
 FFn = test1$FFn;  # Store F matrix from Ndrawn     N*K*Ndrawn
