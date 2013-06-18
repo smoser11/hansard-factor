159ec6a7383e997dd56d30494c50d670fd2a4b7d
liang <sallylia@gmail.com>
2013-06-18 15:13:32 -0500

    detailed R code

--- Rcode/Example.R
+++ Rcode/Example.R
@@ -1,5 +1,7 @@
+# load data
 load("01-1803CorpDTM4546.RData")
 
+# load package
 library(NBfactor)
 
 ## preprocessing YY: doc*term matrix
@@ -13,6 +15,7 @@ for(index in 1:totl){
   YYj = as.numeric(DTM[2]$j[index])
   YY[YYi,YYj] = as.numeric(DTM[3]$v[index])
 }
+## YY(i,j) is the number of occurances of word j in document i 
 
 # number of documents
 N = dim(YY)[1];
@@ -24,16 +27,19 @@ P = dim(YY)[2];
 Ntopic = 5;
 K = Ntopic;
 
-# prior
+# Model:
+# YY_ij  ~ NB(h, exp(Psi_ij)/1+exp(Psi_ij)) i = 1...N, j = 1...p
+# Psi_i ~ N(alpha + gamma_i*1 +    B*f_i, sigma^-1) i = 1...N
+#  p*1     p*1             p*1  p*k k*1    p*p
 
 ## Construct Prior Here
-V = diag(1, K)
-V0 = diag(1,K)
-a = 0.01
-niu_a = 10
-niu_alpha = 10
-niu = rep(0.98,K)
-q = rep(0.9,K)
+V = diag(1, K)        # f_i ~ N(0, V) prior for factor
+V0 = diag(1,K)   
+a = 0.01              # gamma_i ~ N(a, 1)
+niu_a = 10            # a~ N(0, niu_a)
+niu_alpha = 10        # alpha_j ~ N(0, niu_alpha)
+niu = rep(0.98,K)     # niu_s~ IG(c_s, c_s*d_s/2)
+q = rep(0.9,K)        # b_js ~ q_s*N(0,niu_s) + (1-q_s)*delta0   sparsity
 h = 1
 c_s = 1
 d_s = 1
@@ -45,16 +51,13 @@ names(testPrior) = c("V","V0","a","niu_a","niu_alpha","niu","q","h","c_s","d_s")
 ## Can start with the final iteration of last run
 ## B = result$BBn[,,Ndraw]...
 
-B = matrix(rnorm(P*K,0,1), nrow=P, ncol=K)
-f = matrix(rnorm(N*K,0,1), nrow=N, ncol=K)
-alpha = rnorm(P, 0, niu_alpha)
-gamma = rnorm(N, 0, 1)
+B = matrix(rnorm(P*K,0,1), nrow=P, ncol=K)    # loading matrix
+f = matrix(rnorm(N*K,0,1), nrow=N, ncol=K)	  # factor matrix
+alpha = rnorm(P, 0, niu_alpha)                # term-vise intercept
+gamma = rnorm(N, 0, 1)                        # document-vise intercept
 
 testInitial = list(B,f,alpha,gamma);
 names(testInitial) = c("B","f","alpha","gamma");
-
-
-
 
 
 ## number of iterations to be burn
@@ -69,6 +72,64 @@ BBn = test1$BBn;  # Store B matrix from Ndrawn     P*K*Ndrawn
 FFn = test1$FFn;  # Store F matrix from Ndrawn     N*K*Ndrawn
 Alfn = test1$Alfn; # Store alpha vector from Ndrawn   P*Ndrawn
 Gamn = test1$Gamn; # Store gamma vector from Ndrawn  N*Ndrawn
+
+
+
+## evaluation
+
+# plot paramter
+if(0){
+  #plot Gammar[par1]
+  par1 = 4
+  par(mfrow=c(3,1))
+  ts.plot(Gamn[par1,],xlab="iterations",ylab="") 
+  acf(Gamn[par1,],main="") # correlation
+  hist(Gamn[par1,],prob=T,main="",xlab="")
+}
+
+if(0){
+ #plot Alfar[par1]
+  par1 = 15
+  par(mfrow=c(3,1))
+  ts.plot(Alfn[par1,],xlab="iterations",ylab="")
+  acf(Alfn[par1,],main="")
+  hist(Alfn[par1,],prob=T,main="",xlab="")
+}
+
+if(0){
+
+ #calculate mean of drawn B's
+  lmB = matrix(0,P,K)
+  #index.drawn = (Nburn+1):Ndrawn
+  #index.drawn=400:1000
+  for(i in 1:P){
+    for(j in 1:K){
+      lmB[i,j] = mean(BBn[i,j,])
+    }
+  }
+
+ # show the most popular terms in par2 th topic
+  par2 = 1
+  test1 = sort(lmB[,par2],decreasing=TRUE,index.return=T)
+
+  MostTerm1=DTM[6][1]$dimnames$Terms[test1$ix[1:25]]
+  print(MostTerm1)
+
+}
+
+# show alf: term-wise intercept
+if(0){
+  #index.drawn = 60:100  
+  meanAlf = rep(0,P)
+  for(i in 1:P){
+  meanAlf[i] = mean(Alfn[i,])
+  }
+  test2 = sort(meanAlf,decreasing=TRUE,index.return=T)
+  MostTerm2=DTM[6][1]$dimnames$Terms[test2$ix[1:25]]
+  MostTerm2
+
+}
+
 
 
 
