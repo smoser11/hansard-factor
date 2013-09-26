866d0f68080a1350323564189d2a187ac49081f8
James Scott <james.scott@mccombs.utexas.edu>
2013-09-26 17:23:10 -0500

    minor

--- multichain.R
+++ multichain.R
@@ -6,7 +6,12 @@ registerDoMC()
 
 load('data67setting.RData')
 
+result67 = CombFactor(ContY67,Logit67,CountY67,testPrior,testInitial,10,200,100)
 
-result67 = foreach(i=1:8) %dopar% {
-		CombFactor(ContY67,Logit67,CountY67,testPrior,testInitial,10,2000,500)
-	}
+# result67 = foreach(i=1:8) %dopar% {
+		# CombFactor(ContY67,Logit67,CountY67,testPrior,testInitial,10,2000,500)
+	# }
+
+
+tx = 1:ncol(result67$Alfn)
+alphastart = apply(result67$Alfn, 1, function(x) lm(x ~ tx)$fitted.values[length(tx)])
